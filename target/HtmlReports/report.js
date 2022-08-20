bc59b258c196549bd5b06bfe9fc727fcc8010378
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2022-08-20 16:23:17 -0500

    Chore: Subiendo proyecto al repositorio

---
+++ target/HtmlReports/report.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/Features/login.feature");
+formatter.feature({
+  "name": "feature to test login functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Checkin login is successfull with valid credentials",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smokeTest"
+    }
+  ]
+});
+formatter.step({
+  "name": "user is on login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinitions.LoginSteps.user_is_on_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enters username and password",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinitions.LoginSteps.user_enters_username_and_password()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "clicks on login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinitions.LoginSteps.clicks_on_login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is navigated to the home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinitions.LoginSteps.user_is_navigated_to_the_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
